de54cab0261ed74bc997700759b69a5db6b28eef
Alvin Mwangi <iamalvinmwangi@gmail.com>
2025-03-15 17:59:38 +0300

    -a

--- amgpython.py
+++ amgpython.py
@@ -1,3 +1,11 @@
 number1 = 22
 number2 = 23
-print(number2)
+#print(number2)
+#print("Hello World!, This is Alvin")
+name = "Alvin"
+#print(name)
+
+
+
+students = ["Alvin", "Cess", "Njoki", "Mwangi", 4, True]
+print(students[2])
